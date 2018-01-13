28bf1b68af3a9ec1faaebd5a1d84ec7f230edbce
William Wong <wgwong@mit.edu>
2018-01-13 17:20:18 -0500

    working location fetching

--- app/src/main/java/com/example/souleni5k/geesewatcher/MainActivity.java
+++ app/src/main/java/com/example/souleni5k/geesewatcher/MainActivity.java
@@ -1,53 +1,125 @@
 package com.example.souleni5k.geesewatcher;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 10;
+    private static final long UPDATE_INTERVAL =
+            MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+    private static final long FASTEST_INTERVAL =
+            MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Activity thisActivity;
+    private LocationCallback mLocationCallback;
+    private Location lastLocation;
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        // Acquire a reference to the system Location Manager
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-        // Define a listener that responds to location updates
-        LocationListener locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                // Called when a new location is found by the network location provider.
-                makeUseOfNewLocation(location);
-            }
-
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-            public void onProviderEnabled(String provider) {}
-
-            public void onProviderDisabled(String provider) {}
-        };
-
-        // Register the listener with the Location Manager to receive location updates
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-
-
         super.onCreate(savedInstanceState);
+        thisActivity = this;
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                lastLocation = locationResult.getLastLocation();
+            }
+        };
+        startLocationUpdates();
+
+        if ( ContextCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( this, new String[] {  Manifest.permission.ACCESS_FINE_LOCATION },
+                    0 );
+        }
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setImageResource(getResources().getIdentifier("canada_goose_icon_round.png", "id", thisActivity.getPackageName()));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                final View thisView = view;
+                try {
+                    if (lastLocation == null) {
+                        mFusedLocationClient.getLastLocation().addOnSuccessListener(thisActivity, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                if (location != null) {
+                                    Snackbar.make(thisView, "Location acquired!", Snackbar.LENGTH_LONG)
+                                            .setAction("Action", null).show();
+                                    Double latitude = location.getLatitude();
+                                    Double longitude = location.getLongitude();
+                                    Log.d("location is ", latitude.toString() + "," + longitude.toString()); //debug
 
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                                    plotLocation(location);
+                                } else {
+                                    Snackbar.make(thisView, "Location missing, please enable Location Services", Snackbar.LENGTH_LONG)
+                                            .setAction("Action", null).show();
+                                }
+                            }
+                        });
+                    } else {
+                        Double latitude = lastLocation.getLatitude();
+                        Double longitude = lastLocation.getLongitude();
+                        Snackbar.make(thisView, "Location acquired!", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                        Log.d("location is ", latitude.toString() + "," + longitude.toString()); //debug
+
+                        plotLocation(lastLocation);
+                    }
+                } catch (SecurityException e) {
+                    Snackbar.make(thisView, "Insufficient permissions, please restart the application", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
             }
         });
     }
@@ -72,5 +144,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void plotLocation(Location loc) {
+
     }
 }

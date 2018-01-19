e016f36601b0702db47194844fba3e8a274297d2
William Wong <wgwong@mit.edu>
2018-01-19 16:42:57 -0500

    higher resolution canada goose icon button; minor refactoring

--- app/src/main/java/com/example/souleni5k/geesewatcher/MainActivity.java
+++ app/src/main/java/com/example/souleni5k/geesewatcher/MainActivity.java
@@ -27,25 +27,16 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-    private static final int MILLISECONDS_PER_SECOND = 1000;
-
-    public static final int UPDATE_INTERVAL_IN_SECONDS = 10;
-    private static final long UPDATE_INTERVAL =
-            MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
-
-    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
-    private static final long FASTEST_INTERVAL =
-            MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
-
     private static float DEFAULT_ZOOM = 18f;
 
     private FusedLocationProviderClient mFusedLocationProviderClient;
@@ -53,33 +44,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private boolean mLocationPermissionGranted;
     private Location mLastKnownLocation;
     private final LatLng mDefaultLocation = new LatLng (42.360091,-71.096354);
-    private Activity thisActivity;
-    private LocationCallback mLocationCallback;
-    //private Location lastLocation;
-
-    /*
-    private void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        LocationRequest mLocationRequest = LocationRequest.create();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(UPDATE_INTERVAL);
-        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
-    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        thisActivity = this;
         setContentView(R.layout.activity_main);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -94,39 +62,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        /*
-        mLocationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                lastLocation = locationResult.getLastLocation();
-            }
-        };
-        startLocationUpdates();
-        */
 
         FloatingActionButton faButton = findViewById(R.id.fab);
         faButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                /*
                 try {
-                    if (lastLocation == null) {
-                        getLocation(view);
+                    if (mLastKnownLocation == null) {
+                        getDeviceLocation();
 
                     } else {
-                        Double latitude = lastLocation.getLatitude();
-                        Double longitude = lastLocation.getLongitude();
-                        Snackbar.make(view, "Location acquired!", Snackbar.LENGTH_LONG)
+                        Double latitude = mLastKnownLocation.getLatitude();
+                        Double longitude = mLastKnownLocation.getLongitude();
+                        Snackbar.make(view, "Location recorded!", Snackbar.LENGTH_LONG)
                                 .setAction("Action", null).show();
-                        Log.d("location is ", latitude.toString() + "," + longitude.toString()); //debug
+                        Log.d("current location is ", latitude.toString() + "," + longitude.toString()); //debug
 
-                        plotLocation(lastLocation);
+                        //plotLocation();
                     }
                 } catch (SecurityException e) {
                     Snackbar.make(view, "Insufficient permissions, please restart the application", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
-                }*/
+                }
             }
         });
     }
@@ -189,24 +147,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    /*
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[],
-                                           @NonNull int[] grantResults) {
-        //mLocationPermissionGranted = false;
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationPermissionGranted = true;
-                }
-            }
-        }
-        updateLocationUI();
-    }*/
-
     private void getDeviceLocation() {
     /*
      * Get the best and most recent location of the device, which may be null in rare
@@ -223,7 +163,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             mLastKnownLocation = (Location) task.getResult();
                             Log.d("debug", "Current Location: " + mLastKnownLocation.getLatitude() + ", " + mLastKnownLocation.getLongitude());
                             LatLng curLoc = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
-                            mMap.addMarker(new MarkerOptions().position(curLoc).title("Current Location"));
+                            mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).alpha(.75f).position(curLoc).title("Current Location"));
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLoc, DEFAULT_ZOOM));
                         } else {
                             Log.d("debug", "Current location is null. Using defaults.");
@@ -239,48 +179,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    /*
-    private void getLocation(View view) {
-        final View thisView = view;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            throw new IllegalStateException();
-        }
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(thisActivity, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    Snackbar.make(thisView, "Location acquired!", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                    Double latitude = location.getLatitude();
-                    Double longitude = location.getLongitude();
-                    Log.d("location is ", latitude.toString() + "," + longitude.toString()); //debug
-
-                    plotLocation(location);
-                } else {
-                    Snackbar.make(thisView, "Location missing, please enable Location Services", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            }
-        });
-    }*/
-
-    /*
-    private void plotLocation(Location loc) {
-
-    }
-    */
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //stylize the map
         try {
             // Customise the styling of the base map using a JSON object defined
             // in a raw resource file.
@@ -298,14 +201,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         updateLocationUI();
 
         getDeviceLocation();
-
-        /*
-        googleMap.setMyLocationEnabled(true);
-        getLocation(this.findViewById(android.R.id.content).getRootView());
-        Log.d("wow", "found location: " + lastLocation.getLatitude() + ", " + lastLocation.getLongitude());
-        LatLng curLoc = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
-        //googleMap.addMarker(new MarkerOptions().position(curLoc).title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(curLoc));
-        */
     }
 }
